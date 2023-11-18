c4b2e221e18bd11d81b8e4a72407162309676be8
Oper <75073299+ghrjs1998@users.noreply.github.com>
2023-11-18 20:11:40 +0900

    [Bronze V] Title: A-B, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.swift
@@ -0,0 +1,11 @@
+// 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+// 입력: 3 2
+import Foundation
+
+let line = readLine()!
+let lineArr = line.split(separator: " ")
+
+let a = Int(lineArr[0])!
+let b = Int(lineArr[1])!
+
+print(a - b)
