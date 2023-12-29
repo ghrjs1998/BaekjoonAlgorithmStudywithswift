ac9899d214f6ce51202be47ef5726f671cf0e258
Oper <75073299+ghrjs1998@users.noreply.github.com>
2023-12-29 20:12:00 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.29 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.swift
@@ -0,0 +1,9 @@
+// 수식을 계산한 값을 return
+import Foundation
+
+func solution(_ my_string:String) -> Int {
+    my_string
+        .replacingOccurrences(of: " - ", with: " + -")
+        .components(separatedBy: " + ")
+        .reduce(0,  { $0 + Int($1)! })
+}
