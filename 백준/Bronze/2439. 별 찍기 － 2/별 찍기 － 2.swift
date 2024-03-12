d1328b79487436c3555ed724538c5b1e6cda2767
Oper <75073299+ghrjs1998@users.noreply.github.com>
2024-03-12 21:02:44 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.swift
@@ -0,0 +1,11 @@
+/*
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+하지만, 오른쪽을 기준
+*/
+import Foundation
+let line = Int(readLine()!)!
+
+for i in 1...line {
+    print(String(repeating: " ", count: line - i), terminator: "")
+    print(String(repeating: "*", count: i))
+}
